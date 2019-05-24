67f232d232ac3ae09bdc5820c7cd9eba65e6ac7e
azamudio19 <adrian.zamudio.smr@gmail.com>
2019-05-24 13:27:41 +0200

    erggeg

---
+++ src/Persona.java
@@ -0,0 +1,42 @@
+import java.sql.Date;
+
+public class Persona {
+	// Atributos
+	private String nombre;
+	private Date fechaNac;
+	private int anno;
+
+	// Constructor
+	public Persona(String nombre, Date fechaNac, int anno) {
+		super();
+		this.nombre = nombre;
+		this.fechaNac = fechaNac;
+		this.anno = anno;
+	}
+
+	//Metodos
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFechaNac() {
+		return fechaNac;
+	}
+
+	public void setFechaNac(Date fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+
+	public int getAnno() {
+		return anno;
+	}
+
+	public void setAnno(int anno) {
+		this.anno = anno;
+	}
+
+}
